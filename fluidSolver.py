a26ed8fc2ab0d83e2f0d70bbbd49fdb857e95b6c
Roshan Samuel <findrjsam@gmail.com>
2020-02-20 19:40:36 +0530

    Added BSD License header and file

--- fluidSolver.py
+++ fluidSolver.py
@@ -1,10 +1,41 @@
 #!/usr/bin/python
 
+####################################################################################################
+# Orion
+# 
+# Copyright (C) 2020, Roshan J. Samuel
+#
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#     3. Neither the name of the copyright holder nor the
+#        names of its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+####################################################################################################
+
 # Import all necessary modules
 import numpy as np
 import h5py as hp
 
-############################################## USER PARAMETERS #######################################
+############################################ USER PARAMETERS #######################################
 
 # Choose the grid sizes from sLst so as to allow for Multigrid operations
 # [2, 4, 6, 10, 18, 34, 66, 130, 258, 514, 1026, 2050]
@@ -61,7 +92,7 @@ pstSm = 40
 # Number of iterations during smoothing in between prolongation operators
 proSm = 30
 
-########################################### END OF USER PARAMETERS ####################################
+######################################## END OF USER PARAMETERS ####################################
 
 # Redefine frequently used numpy object
 npax = np.newaxis
@@ -848,7 +879,7 @@ def main():
     print "Simulation completed"
 
 
-###########################################################################################################################################
+####################################################################################################
 
 # N should be of the form 2^n + 2 so that there will be 2^n + 3 staggered pressure points, including ghost points
 sLst = [2**x + 2 for x in range(12)]
