Corrected array limits of mesh. Debugged uniform-grid divergence calculation. Array limits no longer needed in calculateFD

orion/calculateFD.py
@@ -37,80 +37,75 @@ from orion.globalVars import planar
 import numpy as np
 
 
-# In all the below differentiation functions, Nx, Ny and Nz are the number of points
-# in the domain *excluding the ghost points* along X, Y and Z respectively.
-# The input field for each function will be expected to have Nx + 2, Ny + 2 and Nz + 2
-# points, but the returned matrix will exclude the ghost points.
-
 if planar:
 ############################# 2D VERSIONS #############################
-    def DDXi(inpFld, Nx, Nz):
+    def DDXi(inpFld):
         outFld = np.zeros_like(inpFld)
-        outFld[1:Nx, 1:Nz] = (inpFld[0:Nx-1, 1:Nz] - 2.0*inpFld[1:Nx, 1:Nz] + inpFld[2:Nx+1, 1:Nz])/hx2
+        outFld[1:-1, 1:-1] = (inpFld[0:-2, 1:-1] - 2.0*inpFld[1:-1, 1:-1] + inpFld[2:, 1:-1])/hx2
 
-        return outFld[1:Nx, 1:Nz]
+        return outFld[1:-1, 1:-1]
 
 
-    def DDZt(inpFld, Nx, Nz):
+    def DDZt(inpFld):
         outFld = np.zeros_like(inpFld)
-        outFld[1:Nx, 1:Nz] = (inpFld[1:Nx, 0:Nz-1] - 2.0*inpFld[1:Nx, 1:Nz] + inpFld[1:Nx, 2:Nz+1])/hz2
+        outFld[1:-1, 1:-1] = (inpFld[1:-1, 0:-2] - 2.0*inpFld[1:-1, 1:-1] + inpFld[1:-1, 2:])/hz2
 
-        return outFld[1:Nx, 1:Nz]
+        return outFld[1:-1, 1:-1]
 
 
-    def D_Xi(inpFld, Nx, Nz):
+    def D_Xi(inpFld):
         outFld = np.zeros_like(inpFld)
-        outFld[1:Nx, 1:Nz] = (inpFld[2:Nx+1, 1:Nz] - inpFld[0:Nx-1, 1:Nz])*0.5/hx
+        outFld[1:-1, 1:-1] = (inpFld[2:, 1:-1] - inpFld[0:-2, 1:-1])*0.5/hx
 
-        return outFld[1:Nx, 1:Nz]
+        return outFld[1:-1, 1:-1]
 
 
-    def D_Zt(inpFld, Nx, Nz):
+    def D_Zt(inpFld):
         outFld = np.zeros_like(inpFld)
-        outFld[1:Nx, 1:Nz] = (inpFld[1:Nx, 2:Nz+1] - inpFld[1:Nx, 0:Nz-1])*0.5/hz
+        outFld[1:-1, 1:-1] = (inpFld[1:-1, 2:] - inpFld[1:-1, 0:-2])*0.5/hz
 
-        return outFld[1:Nx, 1:Nz]
+        return outFld[1:-1, 1:-1]
 
 else:
 ############################# 3D VERSIONS #############################
-    def DDXi(inpFld, Nx, Ny, Nz):
+    def DDXi(inpFld):
         outFld = np.zeros_like(inpFld)
-        outFld[1:Nx, 1:Ny, 1:Nz] = (inpFld[0:Nx-1, 1:Ny, 1:Nz] - 2.0*inpFld[1:Nx, 1:Ny, 1:Nz] + inpFld[2:Nx+1, 1:Ny, 1:Nz])/hx2
+        outFld[1:-1, 1:-1, 1:-1] = (inpFld[0:-2, 1:-1, 1:-1] - 2.0*inpFld[1:-1, 1:-1, 1:-1] + inpFld[2:, 1:-1, 1:-1])/hx2
 
-        return outFld[1:Nx, 1:Ny, 1:Nz]
+        return outFld[1:-1, 1:-1, 1:-1]
 
 
-    def DDEt(inpFld, Nx, Ny, Nz):
+    def DDEt(inpFld):
         outFld = np.zeros_like(inpFld)
-        outFld[1:Nx, 1:Ny, 1:Nz] = (inpFld[1:Nx, 0:Ny-1, 1:Nz] - 2.0*inpFld[1:Nx, 1:Ny, 1:Nz] + inpFld[1:Nx, 2:Ny+1, 1:Nz])/hy2
+        outFld[1:-1, 1:-1, 1:-1] = (inpFld[1:-1, 0:-2, 1:-1] - 2.0*inpFld[1:-1, 1:-1, 1:-1] + inpFld[1:-1, 2:, 1:-1])/hy2
 
-        return outFld[1:Nx, 1:Ny, 1:Nz]
+        return outFld[1:-1, 1:-1, 1:-1]
 
 
-    def DDZt(inpFld, Nx, Ny, Nz):
+    def DDZt(inpFld):
         outFld = np.zeros_like(inpFld)
-        outFld[1:Nx, 1:Ny, 1:Nz] = (inpFld[1:Nx, 1:Ny, 0:Nz-1] - 2.0*inpFld[1:Nx, 1:Ny, 1:Nz] + inpFld[1:Nx, 1:Ny, 2:Nz+1])/hz2
+        outFld[1:-1, 1:-1, 1:-1] = (inpFld[1:-1, 1:-1, 0:-2] - 2.0*inpFld[1:-1, 1:-1, 1:-1] + inpFld[1:-1, 1:-1, 2:])/hz2
 
-        return outFld[1:Nx, 1:Ny, 1:Nz]
+        return outFld[1:-1, 1:-1, 1:-1]
 
 
-    def D_Xi(inpFld, Nx, Ny, Nz):
+    def D_Xi(inpFld):
         outFld = np.zeros_like(inpFld)
-        outFld[1:Nx, 1:Ny, 1:Nz] = (inpFld[2:Nx+1, 1:Ny, 1:Nz] - inpFld[0:Nx-1, 1:Ny, 1:Nz])*0.5/hx
+        outFld[1:-1, 1:-1, 1:-1] = (inpFld[2:, 1:-1, 1:-1] - inpFld[0:-2, 1:-1, 1:-1])*0.5/hx
 
-        return outFld[1:Nx, 1:Ny, 1:Nz]
+        return outFld[1:-1, 1:-1, 1:-1]
 
 
-    def D_Et(inpFld, Nx, Ny, Nz):
+    def D_Et(inpFld):
         outFld = np.zeros_like(inpFld)
-        outFld[1:Nx, 1:Ny, 1:Nz] = (inpFld[1:Nx, 2:Ny+1, 1:Nz] - inpFld[1:Nx, 0:Ny-1, 1:Nz])*0.5/hy
+        outFld[1:-1, 1:-1, 1:-1] = (inpFld[1:-1, 2:, 1:-1] - inpFld[1:-1, 0:-2, 1:-1])*0.5/hy
 
-        return outFld[1:Nx, 1:Ny, 1:Nz]
+        return outFld[1:-1, 1:-1, 1:-1]
 
 
-    def D_Zt(inpFld, Nx, Ny, Nz):
+    def D_Zt(inpFld):
         outFld = np.zeros_like(inpFld)
-        outFld[1:Nx, 1:Ny, 1:Nz] = (inpFld[1:Nx, 1:Ny, 2:Nz+1] - inpFld[1:Nx, 1:Ny, 0:Nz-1])*0.5/hz
+        outFld[1:-1, 1:-1, 1:-1] = (inpFld[1:-1, 1:-1, 2:] - inpFld[1:-1, 1:-1, 0:-2])*0.5/hz
 
-        return outFld[1:Nx, 1:Ny, 1:Nz]
+        return outFld[1:-1, 1:-1, 1:-1]
 

orion/meshData.py
@@ -80,38 +80,38 @@ hx2hy2hz2 = hx2*hy2*hz2
 # Maximum number of jacobi iterations
 maxCount = 10*N*M*L
 
-xColl = np.zeros(L)
-yColl = np.zeros(M)
-zColl = np.zeros(N)
-xStag = np.zeros(L-1)
-yStag = np.zeros(M-1)
-zStag = np.zeros(N-1)
+xStag = np.zeros(L)
+yStag = np.zeros(M)
+zStag = np.zeros(N)
+xColl = np.zeros(L-1)
+yColl = np.zeros(M-1)
+zColl = np.zeros(N-1)
 
 # Grid metric arrays. Used by the non uniform solvers
-# Note that the default values of arrays initialized below corresponds to uniform grid values
-xi_xColl = np.ones(L)       #-- dXi/dX at all x-grid nodes
-xixxColl = np.zeros(L)      #-- d2Xi/dX2 at all x-grid nodes
-xix2Coll = np.ones(L)       #-- (dXi/dX)**2 at all x-grid nodes
+# The default values of arrays initialized below corresponds to uniform grid values
+xi_xColl = np.ones_like(xColl)       #-- dXi/dX at all face-centered points along X direction
+xixxColl = np.zeros_like(xColl)      #-- d2Xi/dX2 at all face-centered points along X direction
+xix2Coll = np.ones_like(xColl)       #-- (dXi/dX)**2 at all face-centered points along X direction
 
-xi_xStag = np.ones(L-1)     #-- dXi/dX at all x-grid nodes
-xixxStag = np.zeros(L-1)    #-- d2Xi/dX2 at all x-grid nodes
-xix2Stag = np.ones(L-1)     #-- (dXi/dX)**2 at all x-grid nodes
+xi_xStag = np.ones_like(xStag)       #-- dXi/dX at all cell-centered points along X direction
+xixxStag = np.zeros_like(xStag)      #-- d2Xi/dX2 at all cell-centered points along X direction
+xix2Stag = np.ones_like(xStag)       #-- (dXi/dX)**2 at all cell-centered points along X direction
 
-et_yColl = np.ones(M)       #-- dEt/dY at all y-grid nodes
-etyyColl = np.zeros(M)      #-- d2Et/dY2 at all y-grid nodes
-ety2Coll = np.ones(M)       #-- (dEt/dY)**2 at all y-grid nodes
+et_yColl = np.ones_like(yColl)       #-- dEt/dY at all face-centered points along Y direction
+etyyColl = np.zeros_like(yColl)      #-- d2Et/dY2 at all face-centered points along Y direction
+ety2Coll = np.ones_like(yColl)       #-- (dEt/dY)**2 at all face-centered points along Y direction
 
-et_yStag = np.ones(M-1)     #-- dEt/dY at all y-grid nodes
-etyyStag = np.zeros(M-1)    #-- d2Et/dY2 at all y-grid nodes
-ety2Stag = np.ones(M-1)     #-- (dEt/dY)**2 at all y-grid nodes
+et_yStag = np.ones_like(yStag)       #-- dEt/dY at all cell-centered points along Y direction
+etyyStag = np.zeros_like(yStag)      #-- d2Et/dY2 at all cell-centered points along Y direction
+ety2Stag = np.ones_like(yStag)       #-- (dEt/dY)**2 at all cell-centered points along Y direction
 
-zt_zColl = np.ones(N)       #-- dZt/dZ at all z-grid nodes
-ztzzColl = np.zeros(N)      #-- d2Zt/dZ2 at all z-grid nodes
-ztz2Coll = np.ones(N)       #-- (dZt/dZ)**2 at all z-grid nodes
+zt_zColl = np.ones_like(zColl)       #-- dZt/dZ at all face-centered points along Z direction
+ztzzColl = np.zeros_like(zColl)      #-- d2Zt/dZ2 at all face-centered points along Z direction
+ztz2Coll = np.ones_like(zColl)       #-- (dZt/dZ)**2 at all face-centered points along Z direction
 
-zt_zStag = np.ones(N-1)     #-- dZt/dZ at all z-grid nodes
-ztzzStag = np.zeros(N-1)    #-- d2Zt/dZ2 at all z-grid nodes
-ztz2Stag = np.ones(N-1)     #-- (dZt/dZ)**2 at all z-grid nodes
+zt_zStag = np.ones_like(zStag)       #-- dZt/dZ at all cell-centered points along Z direction
+ztzzStag = np.zeros_like(zStag)      #-- d2Zt/dZ2 at all cell-centered points along Z direction
+ztz2Stag = np.ones_like(zStag)       #-- (dZt/dZ)**2 at all cell-centered points along Z direction
 
 
 def initializeGrid():
@@ -125,14 +125,14 @@ def initializeGrid():
     et = np.linspace(0.0, 1.0, M)
     zt = np.linspace(0.0, 1.0, N)
 
-    # Initialize staggered and collocated grids
-    xColl = [xLen*(1.0 - np.tanh(xBeta*(1.0 - 2.0*i))/np.tanh(xBeta))/2.0 for i in xi]
-    yColl = [yLen*(1.0 - np.tanh(yBeta*(1.0 - 2.0*i))/np.tanh(yBeta))/2.0 for i in et]
-    zColl = [zLen*(1.0 - np.tanh(zBeta*(1.0 - 2.0*i))/np.tanh(zBeta))/2.0 for i in zt]
+    # Initialize face-centered and cell-centered grids
+    xStag = [xLen*(1.0 - np.tanh(xBeta*(1.0 - 2.0*i))/np.tanh(xBeta))/2.0 for i in xi]
+    yStag = [yLen*(1.0 - np.tanh(yBeta*(1.0 - 2.0*i))/np.tanh(yBeta))/2.0 for i in et]
+    zStag = [zLen*(1.0 - np.tanh(zBeta*(1.0 - 2.0*i))/np.tanh(zBeta))/2.0 for i in zt]
 
-    xStag = [xLen*(1.0 - np.tanh(xBeta*(1.0 - 2.0*i))/np.tanh(xBeta))/2.0 for i in [(xi[j] + xi[j+1])/2 for j in range(len(xi) - 1)]]
-    yStag = [yLen*(1.0 - np.tanh(yBeta*(1.0 - 2.0*i))/np.tanh(yBeta))/2.0 for i in [(et[j] + et[j+1])/2 for j in range(len(et) - 1)]]
-    zStag = [zLen*(1.0 - np.tanh(zBeta*(1.0 - 2.0*i))/np.tanh(zBeta))/2.0 for i in [(zt[j] + zt[j+1])/2 for j in range(len(zt) - 1)]]
+    xColl = [xLen*(1.0 - np.tanh(xBeta*(1.0 - 2.0*i))/np.tanh(xBeta))/2.0 for i in [(xi[j] + xi[j+1])/2 for j in range(len(xi) - 1)]]
+    yColl = [yLen*(1.0 - np.tanh(yBeta*(1.0 - 2.0*i))/np.tanh(yBeta))/2.0 for i in [(et[j] + et[j+1])/2 for j in range(len(et) - 1)]]
+    zColl = [zLen*(1.0 - np.tanh(zBeta*(1.0 - 2.0*i))/np.tanh(zBeta))/2.0 for i in [(zt[j] + zt[j+1])/2 for j in range(len(zt) - 1)]]
 
 
 def calculateMetrics():

orion/solvers/fluidSolverUG_d2.py
@@ -123,18 +123,18 @@ def computeNLinDiff_X(U, W):
     global Hx
     global N, L
 
-    Hx[1:L, 1:N+1] = ((fd.DDXi(U, L, N+1) + fd.DDZt(U, L, N+1))*0.5/gv.Re -
-                       fd.D_Xi(U, L, N+1)*U[1:L, 1:N+1] -
-                      0.25*(W[1:L, 0:N] + W[1:L, 1:N+1] + W[2:L+1, 1:N+1] + W[2:L+1, 0:N])*fd.D_Zt(U, L, N+1))
+    Hx[1:L, 1:N+1] = ((fd.DDXi(U) + fd.DDZt(U))*0.5/gv.Re -
+                       fd.D_Xi(U)*U[1:L, 1:N+1] -
+                      0.25*(W[1:L, 0:N] + W[1:L, 1:N+1] + W[2:L+1, 1:N+1] + W[2:L+1, 0:N])*fd.D_Zt(U))
 
 
 def computeNLinDiff_Z(U, W):
     global Hz
     global N, L
 
-    Hz[1:L+1, 1:N] = ((fd.DDXi(W, L+1, N) + fd.DDZt(W, L+1, N))*0.5/gv.Re -
-                       fd.D_Zt(W, L+1, N)*W[1:L+1, 1:N] -
-                      0.25*(U[0:L, 1:N] + U[1:L+1, 1:N] + U[1:L+1, 2:N+1] + U[0:L, 2:N+1])*fd.D_Xi(W, L+1, N))
+    Hz[1:L+1, 1:N] = ((fd.DDXi(W) + fd.DDZt(W))*0.5/gv.Re -
+                       fd.D_Zt(W)*W[1:L+1, 1:N] -
+                      0.25*(U[0:L, 1:N] + U[1:L+1, 1:N] + U[1:L+1, 2:N+1] + U[0:L, 2:N+1])*fd.D_Xi(W))
 
 
 #Jacobi iterative solver for U
@@ -227,8 +227,7 @@ OUTPUT: The maximum value of divergence in double precision
     divMat = np.zeros([L, N])
     for i in range(1, L):
         for k in range(1, N):
-            divMat[i, k] = (U[i, k] - U[i-1, k])/(grid.xColl[i] - grid.xColl[i-1]) + \
-                           (W[i, k] - W[i, k-1])/(grid.zColl[k] - grid.zColl[k-1])
+            divMat[i, k] = (U[i, k] - U[i-1, k])/grid.hx + (W[i, k] - W[i, k-1])/grid.hz
 
     return np.unravel_index(divMat.argmax(), divMat.shape), np.amax(divMat)
 

orion/solvers/fluidSolverUG_d3.py
@@ -137,30 +137,30 @@ def computeNLinDiff_X(U, V, W):
     global Hx
     global N, M, L
 
-    Hx[1:L, 1:M+1, 1:N+1] = ((fd.DDXi(U, L, M+1, N+1) + fd.DDEt(U, L, M+1, N+1) + fd.DDZt(U, L, M+1, N+1))*0.5/gv.Re -
-                              fd.D_Xi(U, L, M+1, N+1)*U[1:L, 1:M+1, 1:N+1] -
-                           0.25*(V[1:L, 0:M, 1:N+1] + V[1:L, 1:M+1, 1:N+1] + V[2:L+1, 1:M+1, 1:N+1] + V[2:L+1, 0:M, 1:N+1])*fd.D_Et(U, L, M+1, N+1) - 
-                           0.25*(W[1:L, 1:M+1, 0:N] + W[1:L, 1:M+1, 1:N+1] + W[2:L+1, 1:M+1, 1:N+1] + W[2:L+1, 1:M+1, 0:N])*fd.D_Zt(U, L, M+1, N+1))
+    Hx[1:L, 1:M+1, 1:N+1] = ((fd.DDXi(U) + fd.DDEt(U) + fd.DDZt(U))*0.5/gv.Re -
+                              fd.D_Xi(U)*U[1:L, 1:M+1, 1:N+1] -
+                           0.25*(V[1:L, 0:M, 1:N+1] + V[1:L, 1:M+1, 1:N+1] + V[2:L+1, 1:M+1, 1:N+1] + V[2:L+1, 0:M, 1:N+1])*fd.D_Et(U) - 
+                           0.25*(W[1:L, 1:M+1, 0:N] + W[1:L, 1:M+1, 1:N+1] + W[2:L+1, 1:M+1, 1:N+1] + W[2:L+1, 1:M+1, 0:N])*fd.D_Zt(U))
 
 
 def computeNLinDiff_Y(U, V, W):
     global Hy
     global N, M, L
 
-    Hy[1:L+1, 1:M, 1:N+1] = ((fd.DDXi(V, L+1, M, N+1) + fd.DDEt(V, L+1, M, N+1) + fd.DDZt(V, L+1, M, N+1))*0.5/gv.Re -
-                              fd.D_Et(V, L+1, M, N+1)*V[1:L+1, 1:M, 1:N+1] -
-                           0.25*(U[0:L, 1:M, 1:N+1] + U[1:L+1, 1:M, 1:N+1] + U[1:L+1, 2:M+1, 1:N+1] + U[0:L, 2:M+1, 1:N+1])*fd.D_Xi(V, L+1, M, N+1) -
-                           0.25*(W[1:L+1, 1:M, 0:N] + W[1:L+1, 1:M, 1:N+1] + W[1:L+1, 2:M+1, 1:N+1] + W[1:L+1, 2:M+1, 0:N])*fd.D_Zt(V, L+1, M, N+1))
+    Hy[1:L+1, 1:M, 1:N+1] = ((fd.DDXi(V) + fd.DDEt(V) + fd.DDZt(V))*0.5/gv.Re -
+                              fd.D_Et(V)*V[1:L+1, 1:M, 1:N+1] -
+                           0.25*(U[0:L, 1:M, 1:N+1] + U[1:L+1, 1:M, 1:N+1] + U[1:L+1, 2:M+1, 1:N+1] + U[0:L, 2:M+1, 1:N+1])*fd.D_Xi(V) -
+                           0.25*(W[1:L+1, 1:M, 0:N] + W[1:L+1, 1:M, 1:N+1] + W[1:L+1, 2:M+1, 1:N+1] + W[1:L+1, 2:M+1, 0:N])*fd.D_Zt(V))
 
 
 def computeNLinDiff_Z(U, V, W):
     global Hz
     global N, M, L
 
-    Hz[1:L+1, 1:M+1, 1:N] = ((fd.DDXi(W, L+1, M+1, N) + fd.DDEt(W, L+1, M+1, N) + fd.DDZt(W, L+1, M+1, N))*0.5/gv.Re -
-                              fd.D_Zt(W, L+1, M+1, N)*W[1:L+1, 1:M+1, 1:N] -
-                           0.25*(U[0:L, 1:M+1, 1:N] + U[1:L+1, 1:M+1, 1:N] + U[1:L+1, 1:M+1, 2:N+1] + U[0:L, 1:M+1, 2:N+1])*fd.D_Xi(W, L+1, M+1, N) -
-                           0.25*(V[1:L+1, 0:M, 1:N] + V[1:L+1, 1:M+1, 1:N] + V[1:L+1, 1:M+1, 2:N+1] + V[1:L+1, 0:M, 2:N+1])*fd.D_Et(W, L+1, M+1, N))
+    Hz[1:L+1, 1:M+1, 1:N] = ((fd.DDXi(W) + fd.DDEt(W) + fd.DDZt(W))*0.5/gv.Re -
+                              fd.D_Zt(W)*W[1:L+1, 1:M+1, 1:N] -
+                           0.25*(U[0:L, 1:M+1, 1:N] + U[1:L+1, 1:M+1, 1:N] + U[1:L+1, 1:M+1, 2:N+1] + U[0:L, 1:M+1, 2:N+1])*fd.D_Xi(W) -
+                           0.25*(V[1:L+1, 0:M, 1:N] + V[1:L+1, 1:M+1, 1:N] + V[1:L+1, 1:M+1, 2:N+1] + V[1:L+1, 0:M, 2:N+1])*fd.D_Et(W))
 
 
 #Jacobi iterative solver for U
@@ -293,9 +293,9 @@ OUTPUT: The maximum value of divergence in double precision
     for i in range(1, L):
         for j in range(1, M):
             for k in range(1, N):
-                divMat[i, j, k] = (U[i, j, k] - U[i-1, j, k])/(grid.xColl[i] - grid.xColl[i-1]) + \
-                                  (V[i, j, k] - V[i, j-1, k])/(grid.yColl[j] - grid.yColl[j-1]) + \
-                                  (W[i, j, k] - W[i, j, k-1])/(grid.zColl[k] - grid.zColl[k-1])
+                divMat[i, j, k] = (U[i, j, k] - U[i-1, j, k])/grid.hx + \
+                                  (V[i, j, k] - V[i, j-1, k])/grid.hy + \
+                                  (W[i, j, k] - W[i, j, k-1])/grid.hz
 
     return np.unravel_index(divMat.argmax(), divMat.shape), np.amax(divMat)
 